7fe450126cb4fc810b42930fd5ef75a7e907a0b7
apoorvapendse <apoorvavpendse@gmail.com>
2023-08-26 12:22:30 +0530

    more options added

--- frontend/src/components/FileDisplay.jsx
+++ frontend/src/components/FileDisplay.jsx
@@ -4,6 +4,8 @@ const FileDisplay = ({ account, contract }) => {
 
     const [images, setImages] = useState([]);
     const [address, setAddress] = useState(account);
+    const [otherAddresses, setOtherAddresses] = useState([]);
+    console.log(account)
 
     const getdata = async () => {
         try {
@@ -17,15 +19,75 @@ const FileDisplay = ({ account, contract }) => {
             alert("access not available for given address")
         }
     }
+    async function addNewAccessor() {
+        try {
+
+            let newAccessor = prompt("Enter New Address");
+            await contract.addAccess(newAccessor);
+            alert("added accessor:", newAccessor);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async function revokeAccessAddress() {
+        try {
+            let addressToRevokeAccess = document.querySelector("select").value
+            let index = addressToRevokeAccess.indexOf(",");
+            console.log("index:", index)
+            let target = addressToRevokeAccess.substring(0, index);
+            console.log(target)
+
+            await contract.revokeAccess(target);
+            alert("removed accessor:", target);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function getAccessibleAddresses() {
+
+        let otherAddresses = await contract.getSharedOwners();
+        setOtherAddresses(otherAddresses)
+        console.log(otherAddresses)
+    }
+
 
     return (
         <>
+            <button onClick={addNewAccessor} style={{
+                margin: "20px",
+                width: "30%",
+                margin: "auto"
+            }}>Add Access Address</button>
+
+
+
+
+            <select type="text" name="" id="getFileAddress" placeholder='enter address'
+                onClick={getAccessibleAddresses}
+                style={{ color: "black", marginLeft: "50%", transform: "translateX(-50%)", width: "40%", textAlign: "center", marginTop: "15px" }} >
+                {otherAddresses.map((item, index) => {
+                    return (
+                        <option style={{ color: "black" }} value={item}>{item}</option>
+                    )
+                })}
+
+
+
+            </select>
+            <button onClick={revokeAccessAddress} style={{
+                margin: "20px",
+                width: "30%",
+                margin: "auto"
+            }}>Remove Access Address</button>
             <div className='filedisplay'>
 
-                {images.map((item) => {
+                {images.map((item, index) => {
                     return (
+                        <a href={item} target='_blank' style={{ height: "100%", width: "100%" }}>
 
-                        <img src={item} className='image' alt="" srcset="" />
+                            <img src={item} key={index} className='image' alt="" srcset="" />
+                        </a>
                     )
                 })}
 
@@ -33,8 +95,24 @@ const FileDisplay = ({ account, contract }) => {
 
 
             </div>
-            <input type="text" name="" id="" placeholder='enter address' value={address} onChange={(e) => { setAddress(e.target.value) }} style={{ color: "black", marginLeft: "50%", transform: "translateX(-50%)", width: "40%", textAlign: "center", marginTop: "15px" }} />
-            <br></br>
+
+
+
+
+
+            <input type="text" name="" id=""
+                placeholder='Enter target address'
+                value={address}
+                onChange={(e) => {
+                    setAddress(e.target.value)
+                }}
+                style={{
+                    color: "black"
+                    , width: "30%",
+                    textAlign: "center"
+                }}
+
+            />
             <button style={{ marginLeft: "50%", transform: "translateX(-50%)", width: "20%", textAlign: "center", marginTop: "15px" }} onClick={getdata}>GET FILES</button>
         </>
     )
